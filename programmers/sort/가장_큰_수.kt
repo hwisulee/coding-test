b0e3008de9067d56abf5d9d3bd81aa450d2c45fb
cham9598 <cham9598@gmail.com>
2023-07-26 16:45:05 +0900

    Add files 23-07-26

---
+++ programmers/sort/가장_큰_수.kt
@@ -0,0 +1,3 @@
+class Solution {
+    fun solution(numbers: IntArray): String = numbers.sortedWith { n1, n2 -> "$n2$n1".compareTo("$n1$n2") }.joinToString("").run { if (this[0] == '0') "0" else this }
+}
